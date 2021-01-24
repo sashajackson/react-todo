db6dfb4a39c5e8204e38919c935672a151d4175c
Sasha Jackson <drejackson@Sashas-MacBook-Pro.local>
2021-01-24 17:37:16 -0600

    added main to package.json

--- backend/index.js
+++ backend/index.js
@@ -30,7 +30,7 @@ app.listen(PORT, function() {
     console.log("Server is running on Port: " + PORT);
   });
 
-  app.get('*', (req, res) => {
+  app.get('/', (req, res) => {
       res.sendFile(path.join(publicPath, 'index.html'));
   })
 
