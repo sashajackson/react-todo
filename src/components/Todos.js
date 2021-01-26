7d109b601947d1edbeec9c9fe4ad8b85b40ae5c2
Sasha Jackson <drejackson@Sashas-MacBook-Pro.local>
2021-01-25 18:27:34 -0600

    run build

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -6,14 +6,19 @@ class Task extends Component {
 
 
     render(){
-        if(Array.isArray(this.props.task)){
-            console.log(this.props.task);
-            let todoArray = this.props.task.filter(val => val.id);
-            return todoArray.map((todo) => (
-                <TodoItem key={todo.id} task={todo} updateRecord ={this.props.updateRecord} markComplete={this.props.markComplete} delTodo={this.props.delTodo} 
-                log={this.props.log} />
-                    ))
-        }
+
+
+            if(Array.isArray(this.props.task)){
+                console.log(this.props.task);
+                let todoArray = this.props.task.filter(val => val.id);
+                return todoArray.map((todo) => (
+                    <TodoItem key={todo.id} task={todo} updateRecord ={this.props.updateRecord} markComplete={this.props.markComplete} delTodo={this.props.delTodo} 
+                    log={this.props.log} />
+                        ))
+            }
+
+            return null;
+
     }
   
 }
