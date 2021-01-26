90e3986d6ea1a94d66970ccab5b840a0ba12d7b0
Sasha Jackson <drejackson@Sashas-MacBook-Pro.local>
2021-01-26 05:28:29 -0600

    remove isarray in todo

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -8,17 +8,18 @@ class Task extends Component {
     render(){
 
 
-            if(Array.isArray(this.props.task)){
+            
                 console.log(this.props.task);
                 let todoArray = this.props.task.filter(val => val.id);
                 console.log('this is props ', todoArray);
+                
                 return todoArray.map((todo) => (
                     <TodoItem key={todo.id} task={todo} updateRecord ={this.props.updateRecord} markComplete={this.props.markComplete} delTodo={this.props.delTodo} 
                     log={this.props.log} />
                         ))
-            }
+            
 
-            return null;
+           
 
     }
   
