7c3185cd6ccee6043c479ab5d5ae3509fc3e0946
Sasha Jackson <drejackson@Sashas-MacBook-Pro.local>
2021-02-05 10:28:13 -0600

    change mainpage

--- src/components/layout/mainPage.js
+++ src/components/layout/mainPage.js
@@ -6,21 +6,21 @@ class Main extends Component {
         return (
             <div>
               
-                <video style={videoStyle} controls autoPlay>
+                {/* <video style={videoStyle} controls autoPlay>
                     <source src="https://firebasestorage.googleapis.com/v0/b/glco-19bf1.appspot.com/o/images%2FPexels%20Videos%201906755.mp4?alt=media&token=2d4dfbd3-7974-4613-9349-837bcf1dd85b" type="video/mp4">
 
                     </source>
-                </video>
-
-                <h2 style={main}>We all get a little busy.</h2>
-                <h5 style={main2}>Keep your group events organized.</h5>
+                </video> */}
+                <img style={imgStyle} src="https://images.pexels.com/photos/5805001/pexels-photo-5805001.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" />
+                <h2 style={main}>Party Rule #1:</h2>
+                <h5 style={main2}>5 people shouldn't bring soda.</h5>
                 <Link to='/signIn' style={main3}>Sign In <i className="fal fa-arrow-right"></i></Link>
             </div>
         )
     }
 }
 
-const videoStyle = {
+const imgStyle = {
     height: '100vh',
     width: '100%',
     objectFit: 'cover',
@@ -28,7 +28,7 @@ const videoStyle = {
 }
 const main = {
     position: 'absolute',
-    top: '2em',
+    top: '1em',
     zIndex: '999',
     color: 'black',
     background: 'white',
@@ -38,7 +38,7 @@ const main = {
 }
 const main2 = {
     position: 'absolute',
-    top: '7em',
+    top: '4em',
     zIndex: '999',
     color: '#7f5fff',
     background: 'white',
